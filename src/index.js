b32a61b208dcfbb7a3d6d0d14f90cb7c909c7e3c
cornel <mufasa030831@gmail.com>
2025-11-06 05:18:32 +0900

    update google analytics

--- src/index.js
+++ src/index.js
@@ -12,7 +12,25 @@ root.render(
   </AppProvider>
 );
 
+function sendToGoogleAnalytics({name, delta, value, id}) {
+  // Assumes the global `gtag()` function exists, see:
+  // https://developers.google.com/analytics/devguides/collection/ga4
+  gtag('event', name, {
+    // Built-in params:
+    value: delta, // Use `delta` so the value can be summed.
+    // Custom params:
+    metric_id: id, // Needed to aggregate events.
+    metric_value: value, // Optional.
+    metric_delta: delta, // Optional.
+
+    // OPTIONAL: any additional params or debug info here.
+    // See: https://web.dev/articles/debug-performance-in-the-field
+    // metric_rating: 'good' | 'needs-improvement' | 'poor',
+    // debug_info: '...',
+    // ...
+  });
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(sendToGoogleAnalytics);
